90acea5a2a16ee7deab35bf31ccb19ece20ccd44
Daniel Sundberg <daniel.sundberg@gmx.com>
2017-12-30 10:38:25 +0100

    Initial commit of android app

---
+++ app/src/main/java/com/example/daniel/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.daniel.myapplication;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        this.requestWindowFeature((int)Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+
+        mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
